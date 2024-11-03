49da4379259fb5b777186e7b01957ae3467f6e85
Maker Zone <dev@makerzone.com.au>
2024-11-03 18:55:01 +1100

    Cleaning up

--- examples/hello-world/hello-world.ino
+++ examples/hello-world/hello-world.ino
@@ -1,7 +1,7 @@
 #include<LCDI2C.h>
 #include<Wire.h>
 
-LCDI2C lcd(0x3F,16,2); //LCD 클래스 초기화
+LCDI2C lcd(0x3F,16,2);
 
 void setup() {
   Serial.begin(9600);
--- src/LCDI2C.h
+++ src/LCDI2C.h
@@ -7,7 +7,6 @@
 #include <Wire.h>
 #include "Arduino.h"
 
-
 // commands
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
@@ -81,7 +80,6 @@ public:
     void createChar(uint8_t, uint8_t[]);
     void setCursor(uint8_t, uint8_t); 
     void setDelayTime(int t);
-    void printHangul(wchar_t* txt, byte startPoint, byte len);
 #if defined(ARDUINO) && ARDUINO >= 100
     virtual size_t write(uint8_t);
 #else
@@ -91,15 +89,15 @@ public:
     void init();
 
 ////compatibility API function aliases
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
+void blink_on();
+void blink_off();
+void cursor_on();
+void cursor_off();
+void setBacklight(uint8_t new_val);
+void load_custom_character(uint8_t char_num, uint8_t *rows);
 void printstr(const char[]);
 
-////Unsupported API functions (not implemented in this library)
+////Unsupported API functions
 uint8_t status();
 void setContrast(uint8_t new_val);
 uint8_t keypad();
@@ -108,11 +106,9 @@ void on();
 void off();
 uint8_t init_bargraph(uint8_t graphtype);
 void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-	 
+void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_row_end);
 
 private:
-        int printing(byte cho, byte jung, byte jong, byte hanCursor, byte charNum);
         void init_priv();
         void send(uint8_t, uint8_t);
         void write4bits(uint8_t);
@@ -126,158 +122,6 @@ private:
         uint8_t _cols;
         uint8_t _rows;
         uint8_t _backlightval;
-        bool isYi(byte key);   
-        bool isU(byte key);
-        bool isSSang(byte key);
-        void mergeChoJung(byte* cho, byte* jung, byte* mergedArr);
-        int enrollNum = 0;
         int delayTime = 500;
-
-        byte space[8] = {
-                B00000,B00000,B00000,B00000,B00000,B00000,B00000,B00000
-        };
-        //초성/종성 자음
-        byte r[8] = {//ㄱ
-                B11111,B00001,B00001,B00001,B00000,B00000,B00000,B00000
-        };
-        byte s[8] = {//ㄴ
-                B10000,B10000,B10000,B11111,B00000,B00000,B00000,B00000
-        };
-        byte e[8] = {//ㄷ
-                B11111,B10000,B10000,B11111,B00000,B00000,B00000,B00000
-        };
-        byte f[8] = {//ㄹ
-                B11111,B00001,B11111,B10000,B11111,B00000,B00000,B00000
-        };
-        byte a[8] = {//ㅁ
-                B11111,B10001,B10001,B11111,B00000,B00000,B00000,B00000
-        };
-        byte q[8] = {//ㅂ
-                B01010,B01110,B01010,B01110,B00000,B00000,B00000,B00000
-        };
-        byte t[8] = {//ㅅ
-                B00100,B00100,B01010,B10001,B00000,B00000,B00000,B00000
-        };
-        byte d[8] = {//ㅇ
-                B01110,B11011,B10001,B11011,B01110,B00000,B00000,B00000
-        };
-        byte w[8] = {//ㅈ
-                B11111,B00100,B01010,B10001,B00000,B00000,B00000,B00000
-        };
-        byte c[8] = {//ㅊ
-                B00100,B11111,B00100,B01010,B10001,B00000,B00000,B00000
-        };
-        byte z[8] = {//ㅋ
-                B11111,B00001,B01111,B00001,B00001,B00000,B00000,B00000
-        };
-        byte x[8] = {//ㅌ
-                B11111,B10000,B11111,B10000,B11111,B00000,B00000,B00000
-        };
-        byte  v[8] = {//ㅍ
-                B11111,B01010,B01010,B11111,B00000,B00000,B00000,B00000
-        };
-        byte g[8] = {//ㅎ
-                B00100,B11111,B00100,B01010,B00100,B00000,B00000,B00000
-        };
-        byte R[8] = {//ㄲ
-                B11111,B00101,B00101,B00101,B00000,B00000,B00000,B00000
-        };
-        byte E[8] = {//ㄸ
-                B11111,B10100,B10100,B11111,B00000,B00000,B00000,B00000
-        };
-        byte Q[8] = {//ㅃ
-                B10101,B11111,B10101,B11111,B00000,B00000,B00000,B00000
-        };
-        byte T[8] = {//ㅆ
-                B01010,B01010,B10101,B10101,B00000,B00000,B00000,B00000
-        };
-        byte W[8] = {//ㅉ
-                B11111,B01010,B10101,B10101,B00000,B00000,B00000,B00000
-        };
-        //ㅡ형 모음
-        byte m[8] = {//ㅡ
-                B00000,B00000,B00000,B00000,B00000,B00000,B11111,B00000
-        };
-        byte h[8] = {//ㅗ
-                B00000,B00000,B00000,B00000,B00000,B00100,B11111,B00000
-        };
-        byte y[8] = {//ㅛ
-                B00000,B00000,B00000,B00000,B00000,B01010,B11111,B00000
-        };
-        byte n[8] = {//ㅜ
-                B00000,B00000,B00000,B00000,B00000,B00000,B11111,B00100
-        };
-        byte b[8] = {//ㅠ   
-                B00000,B00000,B00000,B00000,B00000,B00000,B11111,B01010
-        };
-        //ㅣ형 모음
-        byte k[8] = { //ㅏ
-                B00000,B10000,B10000,B10000,B11000,B10000,B10000,B00000
-        };
-        byte i[8] = { //ㅑ
-                B00000,B10000,B10000,B11000,B10000,B11000,B10000,B00000
-        };
-        byte j[8] = { //ㅓ
-                B00000,B01000,B01000,B01000,B11000,B01000,B01000,B00000
-        };
-        byte u[8] = { //ㅕ
-                B00000,B01000,B01000,B11000,B01000,B11000,B01000,B00000
-        };
-        byte l[8] = { //ㅣ
-                B00000,B10000,B10000,B10000,B10000,B10000,B10000,B00000
-        };
-        byte o[8] = { //ㅐ
-                B00000,B10100,B10100,B10100,B11100,B10100,B10100,B00000
-        };
-        byte O[8] = { //ㅒ
-                B00000,B10100,B10100,B11100,B10100,B11100,B10100,B00000
-        };
-        byte p[8] = {//ㅔ
-                B00000,B01010,B01010,B01010,B11010,B01010,B01010,B00000
-        };
-        byte P[8] = { //ㅖ
-                B00000,B01010,B01010,B11010,B01010,B11010,B01010,B00000
-        };
-        byte* wcCho[19] = {
-                r, R, s, e, 
-                E, f, a, q, 
-                Q, t, T, d, 
-                w, W, c, z, 
-                x, v, g};
-
-        //중성 ㅏ0,ㅐ1,ㅑ2,ㅒ3,ㅓ4,ㅔ5,ㅕ6,ㅖ7,ㅗ8,ㅘ9,ㅙ10,ㅚ11,ㅛ12,ㅜ13,ㅝ14,ㅞ15,ㅟ16,ㅠ17,ㅡ18,ㅢ19,ㅣ20
-        byte* wcJung[21] = {
-                k, o, i, O, 
-                j, p, u, P, 
-                h, space, space, space, 
-                y, n, space, space, 
-                space, b, m, space, l};
-        //'ㅢ'형 인덱싱
-        int wcUi[21][2] = {
-                {0},{0},{0},{0},
-                {0},{0},{0},{0},
-                {0},{8,0},{8,1},{8,20},
-                {0},{0},{13,4},{13,5},
-                {13,20},{0},{0},{18,20},{0}
-        };
-        //종성 공백0,ㄱ1,ㄲ2,ㄳ3,ㄴ4,ㄵ5,ㄶ6,ㄷ7,ㄹ8,ㄺ9,ㄻ10,ㄼ11,ㄽ12,ㄾ13,ㄿ14,ㅀ15,ㅁ16,ㅂ17,ㅄ18,ㅅ19,ㅆ20,ㅇ21,ㅈ22,ㅊ23,ㅋ24,ㅌ25,ㅍ26,ㅎ27
-        byte* wcJong[28] = {
-                space, r, R, space, 
-                s, space, space, e, 
-                f, space, space, space, 
-                space, space, space, space, 
-                a, q, space, t, 
-                T, d, w, c, 
-                z, x, v, g};
-        //어말자음군 인덱싱
-        int wcSSang[28][2] = {
-                {0},{0},{0},{1,19},
-                {0},{4,22},{4,27},{0},
-                {0},{8,1},{8,16},{8,17},
-                {8,19},{8,25},{8,26},{8,27},
-                {0},{0},{17,19},{0},
-                {0},{0},{0},{0},
-                {0},{0},{0},{0}
-        };
 };
 #endif
